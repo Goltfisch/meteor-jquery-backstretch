d845cd7b2beb4ec3c6ea5b544835dbaa337acf72
Philipp Muens <raute1337@gmx.de>
2015-03-27 16:42:22 +0100

    Add jQuery version constraint
    Add jQuery version constraint so that Meteor knows which jQuery-Version the package needs
    to run properly.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'goltfisch:jquery-backstretch',
   summary: 'jQuery Backstretch repackages for Meteor',
-  version: '0.1.0',
+  version: '0.2.0',
   git: 'https://github.com/goltfisch/meteor-jquery-backstretch.git'
 });
 
 Package.onUse(function (api) {
   api.addFiles('backstretch.min.js', 'client');
 
-  api.use('jquery', 'client');
+  api.use('jquery@1.11.3_2', 'client');
 });
